326ed923d26162f39e119b4c6b139571295bc430
arthurcaraujo <arthur-lagamar@hotmail.com>
2023-11-08 20:04:37 -0300

    Add main content

---
+++ src/components/Main.js
@@ -0,0 +1,90 @@
+export default function Main() {
+    return (
+        <main>
+            <figure>
+                <img
+                    alt="Mauricio de Sousa discursando na 17ª Bienal do Livro"
+                    id="main-image"
+                    src="https://i.imgur.com/RtQwZ54.jpg"
+                />
+
+                <figcaption id="img-caption">
+                    <a
+                        href="https://commons.wikimedia.org/wiki/File:Maur%C3%ADcio_de_Sousa_17_Bienal_do_Livro_1.jpg"
+                        target="_blank"
+                    >
+                        Mauricio de Sousa na 17ª Bienal do Livro, em 2015
+                    </a>
+                    {`. Imagem de Tânia Rêgo/ABr - `}
+                    <a
+                        href="https://agenciabrasil.ebc.com.br/educacao/foto/2015-09/17a-bienal-internacional-do-livro-do-rio-de-janeiro"
+                        target="_blank"
+                    >
+                        Agência Brasil
+                    </a>
+                    {`, estando licenciada sob `}
+                    <a
+                        href="https://creativecommons.org/licenses/by/3.0/br/deed.en"
+                        target="_blank"
+                    >
+                        CC BY 3.0 BR
+                    </a>.
+                </figcaption>
+            </figure>
+
+            <h2 tabIndex="0">Um pouco sobre a vida de Mauricio de Sousa:</h2>
+
+            <section>
+                <ul>
+                    <li><strong>1935</strong> - Nasce em Santa Isabel, São Paulo.</li>
+                    <li><strong>1954</strong> - Procura emprego de desenhista em São Paulo, visto que queria atuar profissionalmente na área. No entanto, só consegue uma vaga como repórter policial no antigo jornal <em>Folha da Manhã</em>, onde ilustrava suas reportagens com desenhos que faziam bastante sucesso.</li>
+                    <li><strong>1959</strong> - Começa a desenhar quadrinhos, sendo aprovada a história dos seus primeiros personagens, o cãozinho Bidu e seu dono Franjinha.</li>
+                    <li><strong>1960</strong> - Inspirando-se em um amigo de infância, cria o Cebolinha, tendo aparecido pela primeira vez como personagem secundário nas tiras de Bidu e Franjinha, publicadas no jornal <em>Folha de São Paulo</em>.</li>
+                    <li><strong>1961</strong> - Cria o personagem Cascão, também inspirado em um amigo de infância. Além do mais, cria o Chico Bento, inspirado em seu tio-avô.</li>
+                    <li><strong>1962</strong> - Contratado pelo jornal <em>Tribuna da Imprensa</em>, no Rio de Janeiro, cria o personagem Piteco e sua turma.</li>
+                    <li><strong>1963</strong> - Inspirando-se em sua segunda filha, cria a personagem Mônica, sendo inserida pela primeira vez numa tirinha do Cebolinha. Maurício passa a desenhar para o caderno infantil da <em>Folha de São Paulo</em> e sua carreira deslancha.</li>
+                    <li><strong>1964</strong> - Cria a personagem Magali, inspirada em sua terceira filha de mesmo nome.</li>
+                    <li><strong>1970</strong> - É lançado o primeiro gibi de nome <em tabIndex="0">Mônica e a sua Turma</em>, fazendo bastante sucesso. Desde então várias histórias foram publicadas e novos personagens foram incluídos nos quadrinhos.</li>
+                    <li><strong>1987</strong> - Passa a ilustrar o suplemento infantil de <em>O Estado de S. Paulo</em>, chamado <em>Estadinho</em>, que até hoje publica tiras da Turma da Mônica.</li>
+                    <li><strong>2008</strong> - Cria também <em>Turma da Mônica Jovem</em>, com a Mônica e seus amigos agora adolescentes, em páginas preto e branco, com um estilo visual influenciado pelos mangás.</li>
+                    <li><strong>2011</strong> - Torna-se o primeiro quadrinista a ser membro da Academia Paulista de Letras, ocupando a cadeira nº 24.</li>
+                </ul>
+            </section>
+            <section>
+                <p>
+                    Maurício de Sousa se inspirava em grande parte em sua
+                    família e amigos ao criar os personagens. Hoje ele é um dos
+                    escritores mais famosos da literatura infantil no Brasil.
+                </p>
+
+                <p>
+                    Em todos esses anos de trabalho, já foram publicadas mais
+                    de 1 bilhão de revistas, tendo sido feitas exportações
+                    para mais de 30 países.
+                </p>
+
+                <blockquote
+                    cite="https://www2.senado.leg.br/bdsf/bitstream/handle/id/334474/noticia.htm?sequence=1"
+                    tabIndex="0"
+                >
+                    <p>
+                        "Mônica, Cascão, Cebolinha, Magali, eles todos são
+                        a minha família, são inspirados na minha vida,
+                        então eles são parte do que eu sou."
+                    </p>
+                    <cite>-- Mauricio de Sousa</cite>
+                </blockquote>
+
+                <p id="more-info">
+                    <a
+                        href="https://pt.wikipedia.org/wiki/Mauricio_de_Sousa"
+                        id="wikipedia-link"
+                        target="_blank"
+                    >
+                        Clique aqui
+                    </a> para saber mais sobre este artista incrível!
+                </p>
+            </section>
+        </main>
+    )
+}
