2c97fbb907e1384a22f14a2cb903f80f2f08c48e
arthurcaraujo <arthur-lagamar@hotmail.com>
2023-11-10 20:15:40 -0300

    Include header and footer

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import "./Header.css";
+
+export default function Header() {
+    return (
+        <header>
+            <h1>Mauricio de Sousa</h1>
+            <p>Cartunista brasileiro criador dos quadrinhos da Turma da Mônica</p>
+        </header>
+    )
+}
